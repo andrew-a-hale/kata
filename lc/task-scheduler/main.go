61dc496fae977aa24ea81b98031ed0f5217e9c53
Andrew Hale <andrew.hale003@gmail.com>
2024-03-22 21:33:10 +1000

    proper priority queue

--- lc/task-scheduler/main.go
+++ lc/task-scheduler/main.go
@@ -12,7 +12,6 @@ type Task struct {
 type TaskQueue []*Task
 
 func (h TaskQueue) Len() int           { return len(h) }
-func (h TaskQueue) IsEmpty() bool      { return len(h) == 0 }
 func (h TaskQueue) Less(i, j int) bool { return h[i].priority > h[j].priority }
 func (h TaskQueue) Swap(i, j int) {
 	h[i], h[j] = h[j], h[i]
@@ -29,7 +28,7 @@ func (h *TaskQueue) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
-	*h = old[0 : n-1]
+	*h = old[:n-1]
 	return x
 }
 
@@ -39,44 +38,44 @@ func scheduler(tasks []byte, n int) int {
 		freq[task]++
 	}
 
+	h := make(TaskQueue, len(freq))
+	i := 0
+	for id, priority := range freq {
+		h[i] = &Task{id: id, index: i, priority: priority}
+		i++
+	}
+	heap.Init(&h)
+
 	used := make(map[byte]int)
 	cycles := 0
-	for ; len(freq) > 0; cycles++ {
-		h := make(TaskQueue, len(freq))
-		i := 0
-		for id, priority := range freq {
-			h[i] = &Task{id: id, index: i, priority: priority}
-			i++
-		}
-		heap.Init(&h)
-
-		for !h.IsEmpty() {
+	for len(freq) > 0 {
+		if len(h) > 0 {
 			task, _ := heap.Pop(&h).(*Task)
-			// skip used
-			if used[task.id] > 0 {
-				continue
-			}
 
 			// consume task
 			used[task.id] = n + 1
 			freq[task.id]--
-			if freq[task.id] <= 0 {
+			if freq[task.id] == 0 {
 				delete(freq, task.id)
 			}
-			break
 		}
 
 		// decrement timeout
 		for k := range used {
 			used[k]--
+			if used[k] == 0 && freq[k] > 0 {
+				heap.Push(&h, &Task{id: k, index: -1, priority: freq[k]})
+			}
 		}
+
+		cycles++
 	}
 
 	return cycles
 }
 
 func main() {
-	var tasks []byte = []byte{'A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'E'}
+	var tasks []byte = []byte{'A', 'A', 'A', 'B', 'B', 'B'}
 	var timeout int = 2
 	println(scheduler(tasks, timeout))
 }
