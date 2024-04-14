e449ed3f2045c1df64cddebfc608466b3c27f3ec
Andrew Hale <andrew.hale003@gmail.com>
2024-04-14 22:59:49 +1000

    example calculator

---
+++ calculator/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type op string
+
+const (
+	Quit     op = "q"
+	Plus     op = "+"
+	Minus    op = "-"
+	Multiply op = "*"
+	Divide   op = "/"
+)
+
+func getInput(reader *bufio.Reader) (string, error) {
+	s, err := reader.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("Failed to read input: %v\n", err)
+	}
+
+	s = strings.TrimSpace(s)
+	if op(s) == Quit {
+		fmt.Println("Quitting...")
+		os.Exit(0)
+	}
+
+	return s, nil
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	done := false
+	for !done {
+		var input string
+		fmt.Printf("Please enter a number: \n")
+		input, err := getInput(reader)
+		if err != nil {
+			log.Fatalf("failed to get input: %v\n", err)
+		}
+
+		n1, err := strconv.ParseInt(input, 10, 32)
+		if err != nil {
+			log.Fatalf("failed to get number: %v\n", err)
+		}
+
+		fmt.Printf("Please enter a number: \n")
+		input, err = getInput(reader)
+		if err != nil {
+			log.Fatalf("failed to get input: %v\n", err)
+		}
+
+		n2, err := strconv.ParseInt(input, 10, 32)
+		if err != nil {
+			log.Fatalf("failed to get number: %v\n", err)
+		}
+
+		fmt.Printf("Please enter a operation (+, -, *, /): \n")
+		input, err = getInput(reader)
+		if err != nil {
+			log.Fatalf("failed to get input: %v\n", err)
+		}
+
+		switch op := op(input); op {
+		case Plus:
+			fmt.Printf("%d %s %d = %d\n", n1, op, n2, n1+n2)
+			done = true
+		case Minus:
+			fmt.Printf("%d %s %d = %d\n", n1, op, n2, n1-n2)
+			done = true
+		case Multiply:
+			fmt.Printf("%d %s %d = %d\n", n1, op, n2, n1*n2)
+			done = true
+		case Divide:
+			fmt.Printf("%d %s %d = %d\n", n1, op, n2, n1/n2)
+			done = true
+		default:
+			fmt.Printf("failed to match an operation: %s\n", op)
+			fmt.Printf("Try Again!\n")
+
+		}
+	}
+}
